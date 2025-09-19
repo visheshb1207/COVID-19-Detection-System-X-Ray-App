6f9c2f702b2ea5549d25ca3d1d7e21d2f02e3b1f
Vishesh Bairwa <162631614+visheshb1207@users.noreply.github.com>
2025-09-19 21:14:34 +0530

    Update app.py

--- app.py
+++ app.py
@@ -64,6 +64,6 @@ def upload_file():
 def camera():
     return render_template('camera.html')
 
-if __name__ == '__main__':
-    app.run(debug=True)
-    # app.run(host='0.0.0.0', port=8080)
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))  
+    app.run(host="0.0.0.0", port=port, debug=False)
